1e0254772fa920da59758e5c086c7092ca1016ea
Raohmaru <namoracon@gmail.com>
2025-07-26 18:48:00 +0200

    Fix run coverage before coverallsapp

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        reporters: ['verbose']
-    }
+        reporters: ['verbose'],
+        coverage: {
+            reporters: ['clover', 'lcov']
+        }
+    },
 });
